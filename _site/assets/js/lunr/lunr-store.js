b98aa525855f02b80d8d82e06e8c93a693f737c6
FarahSaeed <farahsaeedsiddiqi@gmail.com>
2018-07-24 16:48:42 +0500

    Enabling comments
    Adding comments functionality

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Image Classification",
         "excerpt":"    ","categories": ["subject_computervision"],
         "tags": [],
-        "url": "http://localhost:4000/ComputerVision/",
-        "teaser":"http://localhost:4000/assets/images/splash.jpg"}]
+        "url": "/ComputerVision/",
+        "teaser":"/assets/images/splash.jpg"}]
