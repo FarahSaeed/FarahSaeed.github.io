ffb15f85889acb384a45d233310f83845ff8951b
FarahSaeed <farahsaeedsiddiqi@gmail.com>
2018-07-31 11:49:54 +0500

    Minor Changes in Image Classification Post
    Added Minor Changes in Image Classification Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Image Classification",
-        "excerpt":"Hi Guys, Hope you’re all doing great. Above video shows Image Classification task accomplished using Convolutional Neural Network. In this post, I’ll be going through the entire process I have adopted to accomplish task. Goal The Goal is to classify images among the following 5 categories: Indoor Outdoor Night Day...","categories": ["subject_computervision"],
+        "excerpt":"Hi Guys, Hope you’re all doing great. Above video shows Image Classification task accomplished using Convolutional Neural Network. In this post, I’ll be going through the entire process I have adopted to accomplish this task. Goal The Goal is to classify images among the following 5 categories: Indoor Outdoor Night...","categories": ["subject_computervision"],
         "tags": [],
         "url": "/ComputerVision/",
         "teaser":"/assets/images/splash.jpg"}]
--- subject_computervision/_posts/2018-07-22-imageclassification.md
+++ subject_computervision/_posts/2018-07-22-imageclassification.md
@@ -63,7 +63,7 @@ font-size: 100%;
 
 Hi Guys, Hope you’re all doing great.
 
-Above video shows Image Classification task accomplished using Convolutional Neural Network. In this post, I’ll be going through the entire process I have adopted to accomplish task. 
+Above video shows Image Classification task accomplished using Convolutional Neural Network. In this post, I’ll be going through the entire process I have adopted to accomplish this task. 
 
 #### Goal
 The Goal is to classify images among the following 5 categories:
